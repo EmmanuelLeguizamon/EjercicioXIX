1e004197ae54bdaf56d2f1d57e49ee190af500c0
EmmanuelLeguizamon <ema.legui.carp@gmail.com>
2021-09-08 20:16:54 -0300

    Agregar archivos de proyecto.

---
+++ EjercicioXIX/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EjercicioXIX
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
